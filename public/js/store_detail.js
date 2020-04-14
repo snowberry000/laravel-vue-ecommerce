a9736a8f4e3b1d5371b7843e4f19d21535eef479
snowberry000 <snowberry912@gmail.com>
2020-04-14 02:06:49 +0200

    fix minor issue

--- public/js/store_detail.js
+++ public/js/store_detail.js
@@ -113,7 +113,7 @@ app.controller('stores_detail', ['$scope', '$http', '$timeout', function ($scope
 
   $scope.checkIsInCart = function(itemId) {
     var isIn = false;
-    if ($scope.order_data || $scope.order_data.items.length === 0) {      
+    if ($scope.order_data && $scope.order_data.items.length === 0) {      
       $scope.order_data.items.forEach(item => {
         if (!isIn) {
           if (item.item_id === parseInt(itemId))
